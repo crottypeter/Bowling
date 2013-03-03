d4f74ad4a9ee21fb8a375c926546cb755e6e2746
Peter Crotty <peter.crotty@playfish.com>
2013-03-03 13:02:47 +0000

    Add Ball type

---
+++ src/bowling/Ball.java
@@ -0,0 +1,15 @@
+package bowling;
+
+public class Ball {
+
+	private int pins;
+
+	public void setPins(int pins) {
+		this.pins = pins;
+	}
+
+	public int getPins() {
+		return pins;
+	}
+
+}
---
+++ test/bowling/BallTest.java
@@ -0,0 +1,20 @@
+package bowling;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+public class BallTest {
+
+	private static final int FOUR = 4;
+
+	@Test
+	public void givenBall_whenPinsSet_thenPinsValueMatches() {
+		Ball ball = new Ball();
+		ball.setPins(FOUR);
+		assertThat(ball.getPins(), is(equalTo(FOUR)));
+	}
+
+}
